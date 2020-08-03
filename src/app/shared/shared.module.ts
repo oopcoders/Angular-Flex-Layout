e023c327e982856ff6fc45a2db36163dffba41bf
Michal Harrison <mlh1477@gmail.com>
2020-08-03 11:33:02 -0400

    Video 3: Use fxLayout and fxLayoutAlign in Angular Flex Layout

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [FooterComponent, HeaderComponent, SidebarComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FlexLayoutModule, RouterModule],
   exports: [FooterComponent, HeaderComponent, SidebarComponent],
 })
 export class SharedModule {}
